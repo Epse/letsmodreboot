5730a7fe2ee80983577a298c4dd5a6e6a25bf5e0
Epse <stef.plet@gmail.com>
2016-06-21 19:08:29 +0200

    renamed the example mod to LetsModReboot

--- src/main/java/me/epse/letsmodreboot/LetsModReboot.java
+++ src/main/java/me/epse/letsmodreboot/LetsModReboot.java
@@ -1,15 +1,16 @@
-package com.example.examplemod;
+package me.epse.letsmodreboot;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = LetsModReboot.MODID, version = LetsModReboot.VERSION, name = LetsModReboot.NAME)
+public class LetsModReboot
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "epseletsmodreboot";
+    public static final String VERSION = "1.9-1.0"; // epse: again: this is mcversion-modversion
+    public static final String NAME = "Let's Mod Reboot";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
