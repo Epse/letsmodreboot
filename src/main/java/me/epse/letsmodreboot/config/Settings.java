1969f1044d56174a7f4d7367498da58c5d864517
Epse <stef.plet@gmail.com>
2016-06-23 23:30:08 +0200

    important message

--- src/main/java/me/epse/letsmodreboot/config/Settings.java
+++ src/main/java/me/epse/letsmodreboot/config/Settings.java
@@ -1,6 +1,10 @@
 package me.epse.letsmodreboot.config;
 
 public class Settings {
+    //<editor-fold desc="FLAME">
+    // static variables: no! they are evil!
+    // learn the programmer meaning of "evil" fag
+    //</editor-fold>
     private static boolean logLess = false;
     public static boolean getLogLess() {
         return logLess;
